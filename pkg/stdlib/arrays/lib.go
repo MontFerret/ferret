b811c2719b6571f1624a27c15f78138a33fd7d4e
Tim Voronov <ziflex@gmail.com>
2018-10-03 16:54:02 -0400

    #9 Added 'FLATTEN' function

--- pkg/stdlib/arrays/lib.go
+++ pkg/stdlib/arrays/lib.go
@@ -4,6 +4,8 @@ import "github.com/MontFerret/ferret/pkg/runtime/core"
 
 func NewLib() map[string]core.Function {
 	return map[string]core.Function{
-		"APPEND": Append,
+		"APPEND":  Append,
+		"FIRST":   First,
+		"FLATTEN": Flatten,
 	}
 }
