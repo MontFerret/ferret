a7e915117f6b31c25392f2f052a90dabfc913949
Tim Voronov <ziflex@gmail.com>
2018-10-03 17:19:07 -0400

    #9 Added 'INTERSECTION' function

---
+++ pkg/stdlib/arrays/intersection_test.go
@@ -0,0 +1,102 @@
+package arrays_test
+
+import (
+	"context"
+	"github.com/MontFerret/ferret/pkg/runtime/core"
+	"github.com/MontFerret/ferret/pkg/runtime/values"
+	"github.com/MontFerret/ferret/pkg/stdlib/arrays"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestIntersection(t *testing.T) {
+	Convey("Should find intersections between 2 arrays", t, func() {
+		arr1 := values.NewArrayWith(
+			values.NewInt(1),
+			values.NewInt(2),
+			values.NewInt(3),
+			values.NewInt(4),
+			values.NewInt(5),
+			values.NewInt(6),
+		)
+
+		arr2 := values.NewArrayWith(
+			values.NewInt(4),
+			values.NewInt(5),
+			values.NewInt(6),
+			values.NewInt(7),
+			values.NewInt(8),
+			values.NewInt(9),
+		)
+
+		out, err := arrays.Intersection(context.Background(), arr1, arr2)
+
+		check := map[int]bool{
+			4: true,
+			5: true,
+			6: true,
+		}
+
+		So(err, ShouldBeNil)
+
+		arr := out.(*values.Array)
+
+		So(arr.Length(), ShouldEqual, 3)
+
+		arr.ForEach(func(value core.Value, idx int) bool {
+			_, exists := check[int(value.(values.Int))]
+
+			So(exists, ShouldBeTrue)
+
+			return true
+		})
+	})
+
+	Convey("Should find intersections between more than 2 arrays", t, func() {
+		arr1 := values.NewArrayWith(
+			values.NewInt(1),
+			values.NewInt(2),
+			values.NewInt(3),
+			values.NewInt(4),
+			values.NewInt(5),
+		)
+
+		arr2 := values.NewArrayWith(
+			values.NewInt(2),
+			values.NewInt(3),
+			values.NewInt(4),
+			values.NewInt(5),
+			values.NewInt(6),
+		)
+
+		arr3 := values.NewArrayWith(
+			values.NewInt(3),
+			values.NewInt(4),
+			values.NewInt(5),
+			values.NewInt(6),
+			values.NewInt(7),
+		)
+
+		out, err := arrays.Intersection(context.Background(), arr1, arr2, arr3)
+
+		check := map[int]bool{
+			3: true,
+			4: true,
+			5: true,
+		}
+
+		So(err, ShouldBeNil)
+
+		arr := out.(*values.Array)
+
+		So(arr.Length(), ShouldEqual, 3)
+
+		arr.ForEach(func(value core.Value, idx int) bool {
+			_, exists := check[int(value.(values.Int))]
+
+			So(exists, ShouldBeTrue)
+
+			return true
+		})
+	})
+}
--- pkg/stdlib/arrays/lib.go
+++ pkg/stdlib/arrays/lib.go
@@ -4,8 +4,9 @@ import "github.com/MontFerret/ferret/pkg/runtime/core"
 
 func NewLib() map[string]core.Function {
 	return map[string]core.Function{
-		"APPEND":  Append,
-		"FIRST":   First,
-		"FLATTEN": Flatten,
+		"APPEND":       Append,
+		"FIRST":        First,
+		"FLATTEN":      Flatten,
+		"INTERSECTION": Intersection,
 	}
 }
