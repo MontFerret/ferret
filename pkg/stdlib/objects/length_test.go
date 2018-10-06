42233b4a9dc17451e80663ab707f49ced60d8837
= <voistinoff.vladimir@yandex.ru>
2018-10-06 14:37:56 +0300

    fix tests according to other tests

--- pkg/stdlib/objects/length_test.go
+++ pkg/stdlib/objects/length_test.go
@@ -1,9 +1,11 @@
-package objects
+package objects_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/MontFerret/ferret/pkg/runtime/values"
+	"github.com/MontFerret/ferret/pkg/stdlib/objects"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
@@ -14,18 +16,28 @@ func TestLength(t *testing.T) {
 			values.NewObjectProperty("Int", values.NewInt(1)),
 			values.NewObjectProperty("String", values.NewString("one")),
 		)
-		objLen := obj.Length()
 
-		So(objLen, ShouldEqual, values.NewInt(2))
-		So(int(objLen), ShouldEqual, 2)
+		objLen, err := objects.Length(context.Background(), obj)
+
+		So(err, ShouldEqual, nil)
+
+		len := objLen.(values.Int)
+
+		So(len, ShouldEqual, values.NewInt(2))
+		So(int(len), ShouldEqual, int(2))
 	})
 
 	Convey("Should return number 0", t, func() {
 		obj := values.NewObject()
-		objLen := obj.Length()
 
-		So(objLen, ShouldEqual, values.NewInt(0))
-		So(int(objLen), ShouldEqual, 0)
+		objLen, err := objects.Length(context.Background(), obj)
+
+		So(err, ShouldEqual, nil)
+
+		len := objLen.(values.Int)
+
+		So(len, ShouldEqual, values.NewInt(0))
+		So(int(len), ShouldEqual, int(0))
 	})
 
 }
