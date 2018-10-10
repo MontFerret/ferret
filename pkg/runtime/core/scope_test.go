2b370c035fc9f24b7e6948bd4dfc657b03b6a23b
esell <esell@esheavyindustries.com>
2018-10-10 10:44:23 -0600

    add unit tests for runtime/core - scope

---
+++ pkg/runtime/core/scope_test.go
@@ -0,0 +1,30 @@
+package core_test
+
+import (
+	"testing"
+
+	"github.com/MontFerret/ferret/pkg/runtime/core"
+	"github.com/MontFerret/ferret/pkg/runtime/values"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestScope(t *testing.T) {
+	Convey("Should match", t, func() {
+		rs, cf := core.NewRootScope()
+
+		So(cf, ShouldNotBeNil)
+
+		s := core.NewScope(rs)
+
+		So(s.HasVariable("a"), ShouldBeFalse)
+
+		s.SetVariable("a", values.NewString("test"))
+
+		So(s.HasVariable("a"), ShouldBeTrue)
+
+		v, err := s.GetVariable("a")
+
+		So(err, ShouldBeNil)
+		So(v, ShouldEqual, "test")
+	})
+}
