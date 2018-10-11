d0c77fccb83e09e392f38261d6d69cf19aa975b6
= <voistinoff.vladimir@yandex.ru>
2018-10-11 15:39:18 +0300

    added Value to Cloneable interface

--- pkg/runtime/core/cloneable.go
+++ pkg/runtime/core/cloneable.go
@@ -1,5 +1,16 @@
 package core
 
+var cloneableTypes = map[Type]bool{
+	ArrayType:  true,
+	ObjectType: true,
+}
+
 type Cloneable interface {
+	Value
 	Clone() Cloneable
+}
+
+func IsCloneable(value Value) bool {
+	_, ok := cloneableTypes[value.Type()]
+	return ok
 }
