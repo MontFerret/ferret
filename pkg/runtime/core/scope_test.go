607906a9490dd73398ef1d91df1cef6cd3132cc6
esell <esell@esheavyindustries.com>
2018-10-11 09:30:12 -0600

    add unit tests for runtime/core - scope

--- pkg/runtime/core/scope_test.go
+++ pkg/runtime/core/scope_test.go
@@ -37,3 +37,33 @@ func TestScope(t *testing.T) {
 		So(cv, ShouldEqual, "test")
 	})
 }
+
+func TestScopeTraversing(t *testing.T) {
+	Convey("Should match", t, func() {
+		rs, cf := core.NewRootScope()
+		So(cf, ShouldNotBeNil)
+
+		s := core.NewScope(rs)
+
+		rs.SetVariable("a", values.NewString("test"))
+		v, err := s.GetVariable("a")
+
+		// root traversal should work
+		So(err, ShouldBeNil)
+		So(v, ShouldEqual, "test")
+
+		s.SetVariable("b", values.NewString("test2"))
+		s2 := core.NewScope(rs)
+		_, err = s2.GetVariable("b")
+
+		// child traversal should fail
+		So(err, ShouldNotBeNil)
+
+		v2, err := s2.GetVariable("a")
+
+		// root traversal should work
+		So(err, ShouldBeNil)
+		So(v2, ShouldEqual, "test")
+
+	})
+}
