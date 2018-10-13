fcb2a7d66d39696fd82ba1de943c6dc6c7a20a5e
= <voistinoff.vladimir@yandex.ru>
2018-10-13 17:43:52 +0300

    change comment for ZIP function

--- pkg/stdlib/objects/zip.go
+++ pkg/stdlib/objects/zip.go
@@ -9,9 +9,9 @@ import (
 /*
  * Returns an object assembled from the separate parameters keys and values.
  * Keys and values must be arrays and have the same length.
- * @params keys (Array of Objects) - result object keys.
- * @params values (Array of Objects) - result object values.
- * @returns (Object) - Object assembled from the separate parameters keys and values.
+ * @params keys (Array of Strings) - an array of strings, to be used as key names in the result.
+ * @params values (Array of Objects) - an array of core.Value, to be used as key values.
+ * @returns (Object) - an object with the keys and values assembled.
  */
 func Zip(_ context.Context, args ...core.Value) (core.Value, error) {
 	return nil, nil
