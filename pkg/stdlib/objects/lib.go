ebea64da37f95e57f9f0c6a5a7ff367cf7e66848
3timeslazy <voistinoff.vladimir@yandex.ru>
2018-10-23 01:24:53 +0300

    Feature/#10 values (#128)
    * added VALUES function
    * updated object.Compare

--- pkg/stdlib/objects/lib.go
+++ pkg/stdlib/objects/lib.go
@@ -4,10 +4,11 @@ import "github.com/MontFerret/ferret/pkg/runtime/core"
 
 func NewLib() map[string]core.Function {
 	return map[string]core.Function{
-		"HAS":   Has,
-		"KEYS":  Keys,
-		"KEEP":  Keep,
-		"MERGE": Merge,
-		"ZIP":   Zip,
+		"HAS":    Has,
+		"KEYS":   Keys,
+		"KEEP":   Keep,
+		"MERGE":  Merge,
+		"ZIP":    Zip,
+		"VALUES": Values,
 	}
 }
