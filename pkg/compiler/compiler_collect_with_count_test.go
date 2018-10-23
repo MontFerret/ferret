1172b264f360d584a67ce071bb65c639e1454047
Tim Voronov <ziflex@gmail.com>
2018-10-22 23:22:03 -0400

    Added extra unit test

--- pkg/compiler/compiler_collect_with_count_test.go
+++ pkg/compiler/compiler_collect_with_count_test.go
@@ -58,4 +58,54 @@ func TestCollectWithCount(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(string(out), ShouldEqual, `[{"gender":"f","genders":2},{"gender":"m","genders":3}]`)
 	})
+
+	Convey("Should count grouped values with multiple keys", t, func() {
+		c := compiler.New()
+
+		prog, err := c.Compile(`
+			LET users = [
+				{
+					active: true,
+					age: 31,
+					gender: "m",
+					married: true
+				},
+				{
+					active: true,
+					age: 25,
+					gender: "f",
+					married: false
+				},
+				{
+					active: true,
+					age: 36,
+					gender: "m",
+					married: false
+				},
+				{
+					active: false,
+					age: 69,
+					gender: "m",
+					married: true
+				},
+				{
+					active: true,
+					age: 45,
+					gender: "f",
+					married: true
+				}
+			]
+			FOR i IN users
+				COLLECT gender = i.gender, married = i.married WITH COUNT INTO genders
+				RETURN {gender, married, genders}
+		`)
+
+		So(err, ShouldBeNil)
+		So(prog, ShouldHaveSameTypeAs, &runtime.Program{})
+
+		out, err := prog.Run(context.Background())
+
+		So(err, ShouldBeNil)
+		So(string(out), ShouldEqual, `[{"gender":"f","genders":1,"married":false},{"gender":"f","genders":1,"married":true},{"gender":"m","genders":1,"married":false},{"gender":"m","genders":2,"married":true}]`)
+	})
 }
