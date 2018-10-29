88c1aab1a5ae7304d4ff4d2c03fa82b4cf4a6ec6
= <voistinoff.vladimir@yandex.ru>
2018-10-29 03:20:06 +0300

    added unit tests for FMT function

--- pkg/stdlib/strings/fmt_test.go
+++ pkg/stdlib/strings/fmt_test.go
@@ -14,60 +14,111 @@ import (
 type testCase struct {
 	Name      string
 	Expected  string
+	Format    string
 	Args      []core.Value
 	ShouldErr bool
-}
-
-func (tc *testCase) Do(t *testing.T) {
-	Convey(tc.Name, t, func() {
-		expected := values.NewString(tc.Expected)
-
-		formatted, err := strings.Fmt(context.Background(), tc.Args...)
-
-		if tc.ShouldErr {
-			So(err, ShouldBeError)
-		} else {
-			So(err, ShouldBeNil)
-		}
-
-		So(formatted, ShouldEqual, expected)
-	})
 }
 
 func TestFmt(t *testing.T) {
 	tcs := []*testCase{
 		&testCase{
-			Name:     `Should return "1"`,
+			Name:     `FMT("{}", 1) return "1"`,
 			Expected: "1",
+			Format:   "{}",
 			Args: []core.Value{
-				values.NewString("{}"),
 				values.NewInt(1),
 			},
 		},
 		&testCase{
-			Name:     `Should return "2 1 1 2"`,
+			Name:     `FMT("{1} {} {0} {}", 1, 2) return "2 1 1 2"`,
 			Expected: "2 1 1 2",
+			Format:   "{1} {} {0} {}",
 			Args: []core.Value{
-				values.NewString("{1} {} {0} {}"),
 				values.NewInt(1),
 				values.NewInt(2),
 			},
 		},
 		&testCase{
-			Name:     `Should return "Hello, World!"`,
-			Expected: "Hello, World!",
+			Name:     `FMT("{1} {} {0} {} {}", 1, 2, 3) return "2 1 1 2 3"`,
+			Expected: "2 1 1 2 3",
+			Format:   "{1} {} {0} {} {}",
 			Args: []core.Value{
-				values.NewString("{2}{1} {0}"),
+				values.NewInt(1),
+				values.NewInt(2),
+				values.NewInt(3),
+			},
+		},
+		&testCase{
+			Name:     `FMT("{2}{1} {0}", "World!", ",", "Hello") return "Hello, World!"`,
+			Expected: "Hello, World!",
+			Format:   "{2}{1} {0}",
+			Args: []core.Value{
 				values.NewString("World!"),
 				values.NewString(","),
 				values.NewString("Hello"),
 			},
 		},
 		&testCase{
-			Name:     `Should return error [1]`,
-			Expected: "{1}",
+			Name:     `FMT({}, {key:"value"}) return "{"key":"value"}"`,
+			Expected: `{"key":"value"}`,
+			Format:   "{}",
+			Args: []core.Value{
+				values.NewObjectWith(
+					values.NewObjectProperty(
+						"key", values.NewString("value"),
+					),
+				),
+			},
+		},
+		&testCase{
+			Name:     `FMT({}, {key:"value"}) return "{"key":"value"}"`,
+			Expected: `{"key":"value","yek":"eulav"}`,
+			Format:   "{}",
+			Args: []core.Value{
+				values.NewObjectWith(
+					values.NewObjectProperty(
+						"key", values.NewString("value"),
+					),
+					values.NewObjectProperty(
+						"yek", values.NewString("eulav"),
+					),
+				),
+			},
+		},
+		&testCase{
+			Name:     `FMT("string") return "string"`,
+			Expected: "string",
+			Format:   "string",
+		},
+		&testCase{
+			Name:     `FMT("string") return "string"`,
+			Expected: "string",
+			Format:   "string",
+			Args: []core.Value{
+				values.NewInt(1),
+			},
+		},
+		&testCase{
+			Name:      `FMT("{}") return error`,
+			Format:    "{}",
+			Args:      []core.Value{},
+			ShouldErr: true,
+		},
+		&testCase{
+			Name:   `FMT("{1}", 10) return error`,
+			Format: "{1}",
 			Args: []core.Value{
 				values.NewInt(10),
+			},
+			ShouldErr: true,
+		},
+		&testCase{
+			Name:   `FMT("{1} {} {0} {}", 1, 2, 3) return error`,
+			Format: "{1} {} {0} {}",
+			Args: []core.Value{
+				values.NewInt(1),
+				values.NewInt(2),
+				values.NewInt(3),
 			},
 			ShouldErr: true,
 		},
@@ -76,4 +127,26 @@ func TestFmt(t *testing.T) {
 	for _, tc := range tcs {
 		tc.Do(t)
 	}
+}
+
+func (tc *testCase) Do(t *testing.T) {
+	Convey(tc.Name, t, func() {
+		var expected core.Value
+
+		expected = values.NewString(tc.Expected)
+
+		args := []core.Value{values.NewString(tc.Format)}
+		args = append(args, tc.Args...)
+
+		formatted, err := strings.Fmt(context.Background(), args...)
+
+		if tc.ShouldErr {
+			So(err, ShouldBeError)
+			expected = values.None
+		} else {
+			So(err, ShouldBeNil)
+		}
+
+		So(formatted, ShouldEqual, expected)
+	})
 }
