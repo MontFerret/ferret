86bcda76ae1344b82663d4827d1c633f6ec65c3b
= <voistinoff.vladimir@yandex.ru>
2018-10-31 23:36:03 +0300

    added DATE_DAYOFWEEK function

--- pkg/stdlib/datetime/lib.go
+++ pkg/stdlib/datetime/lib.go
@@ -4,6 +4,8 @@ import "github.com/MontFerret/ferret/pkg/runtime/core"
 
 func NewLib() map[string]core.Function {
 	return map[string]core.Function{
-		"NOW": Now,
+		"NOW":            Now,
+		"DATE":           Date,
+		"DATE_DAYOFWEEK": DateDayOfWeek,
 	}
 }
