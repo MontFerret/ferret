d87079898d9391aedc29c2cbf9cba83cc4141b5c
= <voistinoff.vladimir@yandex.ru>
2018-10-31 23:20:21 +0300

    add helpers functions

--- pkg/stdlib/datetime/now_test.go
+++ pkg/stdlib/datetime/now_test.go
@@ -1,24 +1,13 @@
 package datetime_test
 
 import (
-	"context"
 	"testing"
-	"time"
+
+	"github.com/MontFerret/ferret/pkg/stdlib/datetime"
 
 	"github.com/MontFerret/ferret/pkg/runtime/core"
 	"github.com/MontFerret/ferret/pkg/runtime/values"
-	"github.com/MontFerret/ferret/pkg/stdlib/datetime"
-
-	. "github.com/smartystreets/goconvey/convey"
 )
-
-type testCase struct {
-	Name      string
-	Expected  core.Value
-	TimeArg   time.Time
-	Args      []core.Value
-	ShouldErr bool
-}
 
 func TestNow(t *testing.T) {
 	tcs := []*testCase{
@@ -33,25 +22,6 @@ func TestNow(t *testing.T) {
 	}
 
 	for _, tc := range tcs {
-		tc.Do(t)
+		tc.Do(t, datetime.Now)
 	}
-}
-
-func (tc *testCase) Do(t *testing.T) {
-	Convey(tc.Name, t, func() {
-		var expected core.Value
-
-		expected = values.NewDateTime(tc.TimeArg)
-
-		dt, err := datetime.Now(context.Background(), tc.Args...)
-
-		if tc.ShouldErr {
-			So(err, ShouldBeError)
-			expected = values.None
-		} else {
-			So(err, ShouldBeNil)
-		}
-
-		So(dt, ShouldEqual, expected)
-	})
 }
