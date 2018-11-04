b8a40325acc7a4ea52493431a0d57b611ad180da
= <voistinoff.vladimir@yandex.ru>
2018-11-05 00:08:13 +0300

    added DATE_DAYS_IN_MONTH function

--- pkg/stdlib/datetime/lib.go
+++ pkg/stdlib/datetime/lib.go
@@ -4,18 +4,19 @@ import "github.com/MontFerret/ferret/pkg/runtime/core"
 
 func NewLib() map[string]core.Function {
 	return map[string]core.Function{
-		"NOW":              Now,
-		"DATE":             Date,
-		"DATE_DAYOFWEEK":   DateDayOfWeek,
-		"DATE_YEAR":        DateYear,
-		"DATE_MONTH":       DateMonth,
-		"DATE_DAY":         DateDay,
-		"DATE_HOUR":        DateHour,
-		"DATE_MINUTE":      DateMinute,
-		"DATE_SECOND":      DateSecond,
-		"DATE_MILLISECOND": DateMillisecond,
-		"DATE_DAYOFYEAR":   DateDayOfYear,
-		"DATE_LEAPYEAR":    DateLeapYear,
-		"DATE_QUARTER":     DateQuarter,
+		"NOW":                Now,
+		"DATE":               Date,
+		"DATE_DAYOFWEEK":     DateDayOfWeek,
+		"DATE_YEAR":          DateYear,
+		"DATE_MONTH":         DateMonth,
+		"DATE_DAY":           DateDay,
+		"DATE_HOUR":          DateHour,
+		"DATE_MINUTE":        DateMinute,
+		"DATE_SECOND":        DateSecond,
+		"DATE_MILLISECOND":   DateMillisecond,
+		"DATE_DAYOFYEAR":     DateDayOfYear,
+		"DATE_LEAPYEAR":      DateLeapYear,
+		"DATE_QUARTER":       DateQuarter,
+		"DATE_DAYS_IN_MONTH": DateDaysInMonth,
 	}
 }
