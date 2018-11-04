0637fb0ab638c71ffb722770f6ebe568ebd54ecb
= <voistinoff.vladimir@yandex.ru>
2018-11-04 17:22:17 +0300

    added DATE_HOUR, DATE_MINUTE and DATE_SECOND functions

---
+++ pkg/stdlib/datetime/minute.go
@@ -0,0 +1,27 @@
+package datetime
+
+import (
+	"context"
+
+	"github.com/MontFerret/ferret/pkg/runtime/core"
+	"github.com/MontFerret/ferret/pkg/runtime/values"
+)
+
+// DateMinute returns the minute of date as a number.
+// @params date (DateTime) - source DateTime.
+// @return (Int) - a minute number.
+func DateMinute(_ context.Context, args ...core.Value) (core.Value, error) {
+	err := core.ValidateArgs(args, 1, 1)
+	if err != nil {
+		return values.None, err
+	}
+
+	err = core.ValidateType(args[0], core.DateTimeType)
+	if err != nil {
+		return values.None, err
+	}
+
+	min := args[0].(values.DateTime).Minute()
+
+	return values.NewInt(min), nil
+}
