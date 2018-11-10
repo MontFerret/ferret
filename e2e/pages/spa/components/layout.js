a788aa006eed66362fd894cbc68d75c1218bbbfd
Tim Voronov <ziflex@gmail.com>
2018-11-09 23:34:52 -0500

    Updated styles

--- e2e/pages/spa/components/layout.js
+++ e2e/pages/spa/components/layout.js
@@ -1,42 +1,27 @@
 const e = React.createElement;
 const NavLink = ReactRouterDOM.NavLink;
 
-// <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-//     <a class="navbar-brand" href="#">Top navbar</a>
-// <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-//     <span class="navbar-toggler-icon"></span>
-//     </button>
-//     <div class="collapse navbar-collapse" id="navbarCollapse">
-//     <ul class="navbar-nav mr-auto">
-//     <li class="nav-item active">
-//     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-// </li>
-// <li class="nav-item">
-//     <a class="nav-link" href="#">Link</a>
-//     </li>
-//     <li class="nav-item">
-//     <a class="nav-link disabled" href="#">Disabled</a>
-//     </li>
-//     </ul>
-//     <form class="form-inline mt-2 mt-md-0">
-//     <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-//     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//     </form>
-//     </div>
-//     </nav>
-
 export default function Layout({ children }) {
-    return e("div", { className: "cover-container d-flex w-100 h-100 p-3 mx-auto flex-column"}, [
-        e("header", null, [
-            e("div", { className: "inner" }, [
-                e("h3", { className: "masthead-brand"}, "Ferret"),
-                e("nav", {className: "nav nav-masthead justify-content-center"}, [
-                    e(NavLink, { className: "nav-link", to: "/forms"}, "Forms"),
-                    e(NavLink, { className: "nav-link", to: "/navigation"}, "Navigation"),
-                    e(NavLink, { className: "nav-link", to: "/events"}, "Events")
+    return e("div", null, [
+        e("nav", { className: "navbar navbar-expand-md navbar-dark bg-dark mb-4" }, [
+            e(NavLink, { className: "navbar-brand", to: "/"}, "Ferret"),
+            e("button", { className: "navbar-toggler", type: "button"}, [
+                e("span", { className: "navbar-toggler-icon" })
+            ]),
+            e("div", { className: "collapse navbar-collapse" }, [
+                e("ul", { className: "navbar-nav mr-auto" }, [
+                    e("li", { className: "nav-item"}, [
+                        e(NavLink, { className: "nav-link", to: "/forms" }, "Forms")
+                    ]),
+                    e("li", { className: "nav-item"}, [
+                        e(NavLink, { className: "nav-link", to: "/navigation" }, "Navigation")
+                    ]),
+                    e("li", { className: "nav-item"}, [
+                        e(NavLink, { className: "nav-link", to: "/events" }, "Events")
+                    ])
                 ])
             ])
         ]),
-        e("main", { className: "inner"}, children)
+        e("main", { className: "container"}, children)
     ])
 }
--- e2e/pages/spa/components/pages/index.js
+++ e2e/pages/spa/components/pages/index.js
@@ -1,9 +1,8 @@
 const e = React.createElement;
-const Fragment = React.Fragment;
 
 export default function IndexPage() {
-    return e(Fragment, null, [
-        e("h1", { className: "cover-heading" }, "Welcome to Ferret E2E test page!"),
+    return e("div", { className: "jumbotron" }, [
+        e("h1", null, "Welcome to Ferret E2E test page!"),
         e("p", { className: "lead" }, "It has several pages for testing different possibilities of the library")
     ])
 }
