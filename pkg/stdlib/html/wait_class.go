33c55f54d13380ea47e8746d2affad6841f485cb
Tim Voronov <ziflex@gmail.com>
2019-02-20 17:37:31 -0500

    Fixed WAIT_CLASS timeout

--- pkg/stdlib/html/wait_class.go
+++ pkg/stdlib/html/wait_class.go
@@ -2,8 +2,6 @@ package html
 
 import (
 	"context"
-	"time"
-
 	"github.com/MontFerret/ferret/pkg/drivers"
 	"github.com/MontFerret/ferret/pkg/runtime/core"
 	"github.com/MontFerret/ferret/pkg/runtime/values"
@@ -73,7 +71,7 @@ func WaitClass(ctx context.Context, args ...core.Value) (core.Value, error) {
 			timeout = args[3].(values.Int)
 		}
 
-		ctx, fn := context.WithTimeout(ctx, time.Duration(timeout))
+		ctx, fn := waitTimeout(ctx, timeout)
 		defer fn()
 
 		return values.None, doc.WaitForClassBySelector(ctx, selector, class)
