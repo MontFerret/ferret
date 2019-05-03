7bbfe84b03ff9d0346f9f6c6e2b0cb1d474a845d
Владимир Фетисов <fetisov@sports.ru>
2019-05-03 23:47:28 +0300

    fix imports

--- pkg/drivers/http/document_test.go
+++ pkg/drivers/http/document_test.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/MontFerret/ferret/pkg/drivers"
 	"github.com/MontFerret/ferret/pkg/drivers/http"
-	"github.com/MontFerret/ferret/pkg/runtime/core"
 
 	"github.com/PuerkitoBio/goquery"
 	. "github.com/smartystreets/goconvey/convey"
