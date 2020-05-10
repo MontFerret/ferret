7f5229e7feb9188405c486c65ee5990ce20875f2
Adam Korczynski <adam@adalogics.com>
2020-05-10 15:20:23 +0100

    Formatting updates

--- pkg/compiler/fuzz/fuzz.go
+++ pkg/compiler/fuzz/fuzz.go
@@ -4,10 +4,11 @@ import (
 	"github.com/MontFerret/ferret/pkg/compiler"
 )
 
+// This function is our fuzzer
 func Fuzz(data []byte) int {
 	c := compiler.New()
 	p, err := c.Compile(string(data))
-	if err!=nil || p==nil{
+	if err != nil || p == nil{
 		return 0
 	}
 	return 1
