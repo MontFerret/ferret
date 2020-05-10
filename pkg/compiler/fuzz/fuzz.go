d3e782a8bf9a4df5090fa70acecbd293f10c72cf
Adam Korczynski <adam@adalogics.com>
2020-05-10 18:06:01 +0100

    Attempting to fix linter error

--- pkg/compiler/fuzz/fuzz.go
+++ pkg/compiler/fuzz/fuzz.go
@@ -4,11 +4,14 @@ import (
 	"github.com/MontFerret/ferret/pkg/compiler"
 )
 
-// This function is our fuzzer
+// Below function is our fuzzer.
+// The fuzzer is run on oss-fuzz.
+// Link to the project on oss-fuzz
+// will be added once the project is up.
 func Fuzz(data []byte) int {
 	c := compiler.New()
 	p, err := c.Compile(string(data))
-	if err != nil || p == nil{
+	if err != nil || p == nil {
 		return 0
 	}
 	return 1
