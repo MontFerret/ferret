18e1a597af5363347a3024b6fd9d91250feac802
Tim Voronov <ziflex@gmail.com>
2020-05-15 23:49:20 -0400

    Added tests for testing.Gt

--- pkg/stdlib/testing/gt.go
+++ pkg/stdlib/testing/gt.go
@@ -13,7 +13,7 @@ import (
 // @param (String) - Message to display on error.
 var Gt = Assertion{
 	DefaultMessage: func(args []core.Value) string {
-		return fmt.Sprintf("%s to be %s %s", args[0], GreaterOp, args[1])
+		return fmt.Sprintf("be %s [%s] %s", GreaterOp, args[1].Type(), args[1])
 	},
 	MinArgs: 2,
 	MaxArgs: 3,
