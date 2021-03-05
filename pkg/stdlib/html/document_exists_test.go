a27992396c29dde087f7256d09c4caa847da2a95
Tim Voronov <ziflex@gmail.com>
2021-03-05 10:47:15 -0500

    Added addition unit tests

--- pkg/stdlib/html/document_exists_test.go
+++ pkg/stdlib/html/document_exists_test.go
@@ -12,6 +12,25 @@ import (
 
 func TestDocumentExists(t *testing.T) {
 	Convey("DOCUMENT_EXISTS", t, func() {
+		Convey("Should return error if url is not a string", func() {
+			_, err := html.DocumentExists(context.Background(), values.None)
+
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Should return error if options is not an object", func() {
+			_, err := html.DocumentExists(context.Background(), values.NewString("http://fsdfsdfdsdsf.fdf"), values.None)
+
+			So(err, ShouldNotBeNil)
+		})
+
+		Convey("Should return error if headers is not an object", func() {
+			opts := values.NewObjectWith(values.NewObjectProperty("headers", values.None))
+			_, err := html.DocumentExists(context.Background(), values.NewString("http://fsdfsdfdsdsf.fdf"), opts)
+
+			So(err, ShouldNotBeNil)
+		})
+
 		Convey("Should return 'false' when a website does not exist by a given url", func() {
 			out, err := html.DocumentExists(context.Background(), values.NewString("http://fsdfsdfdsdsf.fdf"))
 
