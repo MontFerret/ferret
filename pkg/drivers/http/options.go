9e489741ebd1b0b7939307b1e7ccb5a8a1283c55
Roman <bundlecode@gmail.com>
2021-12-10 00:54:55 +0300

    feat(http): add Timeout

--- pkg/drivers/http/options.go
+++ pkg/drivers/http/options.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	stdhttp "net/http"
+	"time"
 
 	"github.com/gobwas/glob"
 	"github.com/sethgrid/pester"
@@ -12,6 +13,7 @@ import (
 var (
 	DefaultConcurrency = 1
 	DefaultMaxRetries  = 5
+	DefaultTimeout     = time.Second * 30
 )
 
 type (
@@ -29,6 +31,7 @@ type (
 		Concurrency     int
 		HTTPCodesFilter []compiledStatusCodeFilter
 		HTTPTransport   *stdhttp.Transport
+		Timeout         time.Duration
 	}
 )
 
@@ -141,5 +144,11 @@ func WithAllowedHTTPCodes(httpCodes []int) Option {
 func WithCustomTransport(transport *stdhttp.Transport) Option {
 	return func(opts *Options) {
 		opts.HTTPTransport = transport
+	}
+}
+
+func WithTimeout(duration time.Duration) Option {
+	return func(opts *Options) {
+		opts.Timeout = duration
 	}
 }
