59deed3de16b0b29f526b5c6b1ba2ee595a07377
Roman <bundlecode@gmail.com>
2021-12-28 22:20:29 +0300

    feat(test): add test for simpleHTTPRequest with body

--- pkg/stdlib/html/tests/document_test.go
+++ pkg/stdlib/html/tests/document_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	h "net/http"
 	"net/http/httptest"
@@ -20,45 +21,96 @@ import (
 
 func TestOpenWithSimpleHTTPRequest(t *testing.T) {
 	Convey("open with simpleRequest params", t, func() {
-		expected := `{"url":"","status_code":200,"status":"200 OK","headers":{"Set-Cookie":"SampleCookie=sample","Some-Header":"value"},"body":"PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PC9oZWFkPjxib2R5PnNvbWUgZGF0YTwvYm9keT48L2h0bWw+","response_time":0,"cookies":{"SampleCookie":{"domain":"","expires":"0001-01-01T00:00:00Z","http_only":false,"max_age":0,"name":"SampleCookie","path":"","same_site":"","secure":false,"value":"sample"}}}`
+		Convey("method GET", func() {
+			expected := `{"url":"","status_code":200,"status":"200 OK","headers":{"Set-Cookie":"SampleCookie=sample","Some-Header":"value"},"body":"PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PC9oZWFkPjxib2R5PnNvbWUgZGF0YTwvYm9keT48L2h0bWw+","response_time":0,"cookies":{"SampleCookie":{"domain":"","expires":"0001-01-01T00:00:00Z","http_only":false,"max_age":0,"name":"SampleCookie","path":"","same_site":"","secure":false,"value":"sample"}}}`
 
-		httpmock.Activate()
-		defer httpmock.DeactivateAndReset()
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
 
-		httpmock.RegisterResponder("GET", "http://localhost:1111", func(request *h.Request) (resp *h.Response, err error) {
-			rr := &httptest.ResponseRecorder{}
+			httpmock.RegisterResponder("GET", "http://localhost:1111", func(request *h.Request) (resp *h.Response, err error) {
+				rr := &httptest.ResponseRecorder{}
 
-			rr.Body = bytes.NewBufferString(`<!DOCTYPE html><html><head></head><body>some data</body></html>`)
-			h.SetCookie(rr, &h.Cookie{Name: "SampleCookie", Value: "sample", HttpOnly: false})
+				rr.Body = bytes.NewBufferString(`<!DOCTYPE html><html><head></head><body>some data</body></html>`)
+				h.SetCookie(rr, &h.Cookie{Name: "SampleCookie", Value: "sample", HttpOnly: false})
 
-			resp = rr.Result()
-			resp.Header.Add("Some-Header", "value")
-			resp.Request = request
-			resp.StatusCode = h.StatusOK
+				resp = rr.Result()
+				resp.Header.Add("Some-Header", "value")
+				resp.Request = request
+				resp.StatusCode = h.StatusOK
 
-			return
+				return
 
-		})
+			})
 
-		c := compiler.New()
+			c := compiler.New()
 
-		p, err := c.Compile(`
+			p, err := c.Compile(`
         let doc = document("http://localhost:1111", {simpleRequest: {method: "GET"}})
         return doc
 `)
 
-		So(err, ShouldBeNil)
-		So(p, ShouldHaveSameTypeAs, &runtime.Program{})
+			So(err, ShouldBeNil)
+			So(p, ShouldHaveSameTypeAs, &runtime.Program{})
 
-		ctx := drivers.WithContext(context.Background(),
-			http.NewDriver(),
-			drivers.AsDefault())
+			ctx := drivers.WithContext(context.Background(),
+				http.NewDriver(),
+				drivers.AsDefault())
 
-		out, err := p.Run(ctx)
+			out, err := p.Run(ctx)
 
-		So(err, ShouldBeNil)
-		fmt.Println()
+			So(err, ShouldBeNil)
+			fmt.Println()
 
-		So(string(out), ShouldEqual, expected)
+			So(string(out), ShouldEqual, expected)
+		})
+
+		Convey("method POST. Request should exists body", func() {
+			expected := `{"url":"","status_code":200,"status":"200 OK","headers":{"Set-Cookie":"SampleCookie=sample","Some-Header":"value"},"body":"PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PC9oZWFkPjxib2R5PnNvbWUgZGF0YTwvYm9keT48L2h0bWw+","response_time":0,"cookies":{"SampleCookie":{"domain":"","expires":"0001-01-01T00:00:00Z","http_only":false,"max_age":0,"name":"SampleCookie","path":"","same_site":"","secure":false,"value":"sample"}}}`
+
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			httpmock.RegisterResponder("POST", "http://localhost:1111", func(request *h.Request) (resp *h.Response, err error) {
+				if request.Body == nil {
+					return nil, errors.New("request Body doesn't exists")
+				}
+
+				rr := &httptest.ResponseRecorder{}
+
+				rr.Body = bytes.NewBufferString(`<!DOCTYPE html><html><head></head><body>some data</body></html>`)
+				h.SetCookie(rr, &h.Cookie{Name: "SampleCookie", Value: "sample", HttpOnly: false})
+
+				resp = rr.Result()
+				resp.Header.Add("Some-Header", "value")
+				resp.Request = request
+				resp.StatusCode = h.StatusOK
+
+				return
+
+			})
+
+			c := compiler.New()
+
+			p, err := c.Compile(`
+        let req = to_binary("some input param")
+        let doc = document("http://localhost:1111", {simpleRequest: {method: "POST", body: req}})
+        return doc
+`)
+
+			So(err, ShouldBeNil)
+			So(p, ShouldHaveSameTypeAs, &runtime.Program{})
+
+			ctx := drivers.WithContext(context.Background(),
+				http.NewDriver(),
+				drivers.AsDefault())
+
+			out, err := p.Run(ctx)
+
+			So(err, ShouldBeNil)
+			fmt.Println()
+
+			So(string(out), ShouldEqual, expected)
+		})
+
 	})
 }
