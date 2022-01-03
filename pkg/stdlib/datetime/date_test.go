a97a003b5cfdc8dd8857c64004446665abb91666
Tim Voronov <ziflex@gmail.com>
2022-01-02 23:17:07 -0500

    Added support of parsing DateTime with custom layout

--- pkg/stdlib/datetime/date_test.go
+++ pkg/stdlib/datetime/date_test.go
@@ -2,6 +2,7 @@ package datetime_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/MontFerret/ferret/pkg/runtime/core"
 	"github.com/MontFerret/ferret/pkg/runtime/values"
@@ -10,29 +11,32 @@ import (
 )
 
 func TestDate(t *testing.T) {
+	now := time.Now()
+
 	tcs := []*testCase{
-		&testCase{
-			Name:     "When more than 1 arguments",
+		{
+			Name:     "When more than 2 arguments",
 			Expected: values.None,
 			Args: []core.Value{
-				values.NewString("string"),
-				values.NewInt(0),
+				values.NewString(time.Now().Format(time.RFC3339)),
+				values.NewString(time.RFC3339),
+				values.NewString("foo"),
 			},
 			ShouldErr: true,
 		},
-		&testCase{
+		{
 			Name:      "When 0 arguments",
 			Expected:  values.None,
 			Args:      []core.Value{},
 			ShouldErr: true,
 		},
-		&testCase{
-			Name:      "When argument isn't DateTime",
+		{
+			Name:      "When first argument isn't string",
 			Expected:  values.None,
 			Args:      []core.Value{values.NewInt(0)},
 			ShouldErr: true,
 		},
-		&testCase{
+		{
 			Name:     "When incorrect timeStrings",
 			Expected: values.None,
 			Args: []core.Value{
@@ -40,12 +44,30 @@ func TestDate(t *testing.T) {
 			},
 			ShouldErr: true,
 		},
-		&testCase{
-			Name:     "When correct timeString in RFC3339 format",
+		{
+			Name:     "When string is in default format",
 			Expected: mustDefaultLayoutDt("1999-02-07T15:04:05Z"),
 			Args: []core.Value{
 				values.NewString("1999-02-07T15:04:05Z"),
 			},
+		},
+		{
+			Name:     "When second argument isn't string",
+			Expected: values.None,
+			Args: []core.Value{
+				values.NewString("1999-02-07T15:04:05Z"),
+				values.NewInt(1),
+			},
+			ShouldErr: true,
+		},
+		{
+			Name:     "When string is in custom format",
+			Expected: mustLayoutDt(time.RFC822, now.Format(time.RFC822)),
+			Args: []core.Value{
+				values.NewString(now.Format(time.RFC822)),
+				values.NewString(time.RFC822),
+			},
+			ShouldErr: false,
 		},
 	}
 
