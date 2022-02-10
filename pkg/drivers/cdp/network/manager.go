70a8ef790bfe08c72338e9427389d064019e4d4a
Roman <bundlecode@gmail.com>
2022-02-10 23:42:07 +0300

    fix go routines leakage (#726)
    * fix go routines leakage
    
    * Update manager.go
    
    fmt
    
    Co-authored-by: Tim Voronov <ziflex@users.noreply.github.com>

--- pkg/drivers/cdp/network/manager.go
+++ pkg/drivers/cdp/network/manager.go
@@ -494,16 +494,15 @@ func (m *Manager) NavigateBack(ctx context.Context, skip values.Int) (values.Boo
 }
 
 func (m *Manager) WaitForNavigation(ctx context.Context, opts WaitEventOptions) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
 	stream, err := m.OnNavigation(ctx)
-
 	if err != nil {
 		return err
 	}
 
 	defer stream.Close(ctx)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
 	for evt := range stream.Read(ctx) {
 		if err := ctx.Err(); err != nil {
