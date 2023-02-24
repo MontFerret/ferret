063e1a51066beea05cd23b382a5a97267e726c33
June Hester <ejune@pm.me>
2023-02-23 22:44:42 -0800

    Rename unused context parameter

--- pkg/stdlib/types/to_binary.go
+++ pkg/stdlib/types/to_binary.go
@@ -10,7 +10,7 @@ import (
 // ToBinary takes an input value of any type and converts it into a binary value.
 // @param {Any} value - Input value of arbitrary type.
 // @return {Binary} - A binary value.
-func ToBinary(ctx context.Context, args ...core.Value) (core.Value, error) {
+func ToBinary(_ context.Context, args ...core.Value) (core.Value, error) {
 	err := core.ValidateArgs(args, 1, 1)
 
 	if err != nil {
