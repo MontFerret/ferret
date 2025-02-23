4ba0c6af1bbf1c66d5c95f91c95896ca8d183055
Tim Voronov <ziflex@gmail.com>
2025-02-23 13:46:17 -0500

    Un-skipped unit tests

--- pkg/compiler/compiler_exec_test.go
+++ pkg/compiler/compiler_exec_test.go
@@ -1334,14 +1334,14 @@ func TestForLimit(t *testing.T) {
 
 func TestForSort(t *testing.T) {
 	RunUseCases(t, []UseCase{
-		SkipCaseArray(`
+		CaseArray(`
 LET strs = ["foo", "bar", "qaz", "abc"]
 
 FOR s IN strs
 	SORT s
 	RETURN s
 `, []any{"abc", "bar", "foo", "qaz"}, "Should sort strings"),
-		SkipCaseArray(`
+		CaseArray(`
 LET users = [
 				{
 					name: "Ron",
@@ -1367,7 +1367,7 @@ LET users = [
 			map[string]any{"name": "Bob", "age": 36, "gender": "m"},
 			map[string]any{"name": "Ron", "age": 31, "gender": "m"},
 		}, "Should sort objects by name (string)"),
-		SkipCaseArray(`
+		CaseArray(`
 LET users = [
 				{
 					active: true,
@@ -1393,7 +1393,7 @@ LET users = [
 			map[string]any{"active": true, "age": 31, "gender": "m"},
 			map[string]any{"active": true, "age": 36, "gender": "m"},
 		}, "Should sort objects by age (int)"),
-		SkipCaseArray(`
+		CaseArray(`
 			LET users = [
 				{
 					active: true,
@@ -1419,7 +1419,7 @@ LET users = [
 			map[string]any{"active": true, "age": 31, "gender": "m"},
 			map[string]any{"active": true, "age": 29, "gender": "f"},
 		}, "Should execute query with DESC SORT statement"),
-		SkipCaseArray(`
+		CaseArray(`
 			LET users = [
 				{
 					active: true,
@@ -1445,7 +1445,7 @@ LET users = [
 			map[string]any{"active": true, "age": 31, "gender": "m"},
 			map[string]any{"active": true, "age": 36, "gender": "m"},
 		}, "Should compile query with ASC SORT statement"),
-		SkipCaseArray(`			LET users = [
+		CaseArray(`			LET users = [
 				{
 					active: true,
 					age: 31,
@@ -1476,7 +1476,7 @@ LET users = [
 				map[string]any{"active": true, "age": 31, "gender": "m"},
 				map[string]any{"active": true, "age": 36, "gender": "m"},
 			}, "Should compile query with SORT statement with multiple expressions"),
-		SkipCaseArray(`
+		CaseArray(`
 			LET users = [
 				{
 					active: true,
