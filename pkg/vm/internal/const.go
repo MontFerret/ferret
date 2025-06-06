176fe8ce4ff52a88b2457f5043ec775af4e9f202
Tim Voronov <ziflex@gmail.com>
2025-06-06 16:34:20 -0400

    Refactor collectors and sorters; introduce Transformers
    Refactor `Collector` into `Transformer` for enhanced flexibility and modularity. Introduce `Sorter` and `MultiSorter` as specialized transformers to handle sorting operations. Streamline VM operations by replacing dataset-based methods with transformer logic. Add encoding/decoding utilities for multiple sorting directions. Optimize `Emit` logic and update related tests.

--- pkg/vm/internal/const.go
+++
@@ -1,8 +0,0 @@
-package internal
-
-import "github.com/MontFerret/ferret/pkg/runtime"
-
-const (
-	SortAsc  runtime.Int = 0
-	SortDesc runtime.Int = 1
-)
---
+++ pkg/vm/internal/sorter_multi.go
@@ -0,0 +1,69 @@
+package internal
+
+import (
+	"context"
+
+	"github.com/MontFerret/ferret/pkg/runtime"
+)
+
+type MultiSorter struct {
+	*runtime.Box[runtime.List]
+	directions []runtime.SortDirection
+	sorted     bool
+}
+
+func NewMultiSorter(directions []runtime.SortDirection) Transformer {
+	return &MultiSorter{
+		Box:        &runtime.Box[runtime.List]{Value: runtime.NewArray(8)},
+		directions: directions,
+		sorted:     false,
+	}
+}
+
+func (s *MultiSorter) Iterate(ctx context.Context) (runtime.Iterator, error) {
+	if !s.sorted {
+		if err := s.sort(ctx); err != nil {
+			return nil, err
+		}
+
+		s.sorted = true
+	}
+
+	iter, err := s.Value.Iterate(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return NewKVIterator(iter), nil
+}
+
+func (s *MultiSorter) Add(ctx context.Context, key, value runtime.Value) error {
+	return s.Value.Add(ctx, NewKV(key, value))
+}
+
+func (s *MultiSorter) sort(ctx context.Context) error {
+	return runtime.SortListWith(ctx, s.Value, func(first, second runtime.Value) int64 {
+		firstKV := first.(*KV)
+		secondKV := second.(*KV)
+
+		firstKVKey := firstKV.Key.(runtime.List)
+		secondKVKey := secondKV.Key.(runtime.List)
+
+		for idx, direction := range s.directions {
+			firstKey, _ := firstKVKey.Get(ctx, runtime.NewInt(idx))
+			secondKey, _ := secondKVKey.Get(ctx, runtime.NewInt(idx))
+			comp := runtime.CompareValues(firstKey, secondKey)
+
+			if comp != 0 {
+				if direction == runtime.SortDirectionAsc {
+					return comp
+				}
+
+				return -comp
+			}
+		}
+
+		return 0
+	})
+}
