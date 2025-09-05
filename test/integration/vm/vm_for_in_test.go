a74ed8d6baa1b5e9a7387db195a9fd2fb23a71af
Tim Voronov <ziflex@gmail.com>
2025-09-04 20:55:44 -0400

    Refactor `FOR-IN` integration tests: improve formatting, add ignored variable case, and enhance test cases for arrays and objects handling.

--- test/integration/vm/vm_for_in_test.go
+++ test/integration/vm/vm_for_in_test.go
@@ -18,47 +18,53 @@ func TestForIn(t *testing.T) {
 	//},
 	RunUseCases(t, []UseCase{
 		CaseArray(`
-FOR i IN 1..5
-	RETURN i
+			FOR i IN 1..5
+				RETURN i
 `, []any{1, 2, 3, 4, 5}),
+		CaseArray(`
+			FOR _ IN 1..5
+				RETURN 1
+`, []any{1, 1, 1, 1, 1}, "Should use ignored variable"),
 		CaseArray(
 			`
-		FOR i IN 1..5
-			LET x = i * 2
-			RETURN x
+			FOR i IN 1..5
+				LET x = i * 2
+				RETURN x
 		`,
 			[]any{2, 4, 6, 8, 10},
 		),
 		CaseArray(
 			`
-		FOR val, counter IN 1..5
-			LET x = val
-			TEST_FN(counter)
-			LET y = counter
-			RETURN [x, y]
+			FOR val, counter IN 1..5
+				LET x = val
+				TEST_FN(counter)
+				LET y = counter
+				RETURN [x, y]
 				`,
 			[]any{[]any{1, 0}, []any{2, 1}, []any{3, 2}, []any{4, 3}, []any{5, 4}},
 		),
 		CaseArray(
-			`FOR i IN [] RETURN i
+			`
+			FOR i IN [] RETURN i
 				`,
 			[]any{},
 		),
 		CaseArray(
-			`FOR i IN [1, 2, 3] RETURN i
+			`
+			FOR i IN [1, 2, 3] RETURN i
 				`,
 			[]any{1, 2, 3},
 		),
-		CaseArray(
-			`FOR i, k IN [1, 2, 3] RETURN k`,
+		CaseArray(`
+			FOR i, k IN [1, 2, 3] RETURN k`,
 			[]any{0, 1, 2},
 		),
-		CaseArray(
-			`FOR i IN ['foo', 'bar', 'qaz'] RETURN i`,
+		CaseArray(`
+			FOR i IN ['foo', 'bar', 'qaz'] RETURN i`,
 			[]any{"foo", "bar", "qaz"},
 		),
-		CaseFn(
-			`FOR i IN {a: 'bar', b: 'foo', c: 'qaz'} RETURN i`,
+		CaseFn(`
+			FOR i IN {a: 'bar', b: 'foo', c: 'qaz'} RETURN i`,
 			func(actual any, expected ...any) string {
 				hashMap := make(map[string]bool)
 				expectedArr := []any{"bar", "foo", "qaz"}
@@ -79,8 +85,8 @@ FOR i IN 1..5
 				return ""
 			},
 		),
-		CaseFn(
-			`FOR i, k IN {a: 'foo', b: 'bar', c: 'qaz'} RETURN k`,
+		CaseFn(`
+			FOR i, k IN {a: 'foo', b: 'bar', c: 'qaz'} RETURN k`,
 			func(actual any, expected ...any) string {
 				hashMap := make(map[string]bool)
 				expectedArr := []any{"a", "b", "c"}
@@ -101,12 +107,12 @@ FOR i IN 1..5
 				return ""
 			},
 		),
-		CaseArray(
-			`FOR i IN [{name: 'foo'}, {name: 'bar'}, {name: 'qaz'}] RETURN i.name`,
+		CaseArray(`
+			FOR i IN [{name: 'foo'}, {name: 'bar'}, {name: 'qaz'}] RETURN i.name`,
 			[]any{"foo", "bar", "qaz"},
 		),
-		CaseArray(
-			`FOR i IN { items: [{name: 'foo'}, {name: 'bar'}, {name: 'qaz'}] }.items RETURN i.name`,
+		CaseArray(`
+			FOR i IN { items: [{name: 'foo'}, {name: 'bar'}, {name: 'qaz'}] }.items RETURN i.name`,
 			[]any{"foo", "bar", "qaz"},
 		),
 	}, vm.WithFunction("TEST_FN", func(ctx context.Context, args ...runtime.Value) (runtime.Value, error) {
