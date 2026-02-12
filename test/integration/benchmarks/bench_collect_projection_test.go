bcff38ab1e0115a0ec80e272850a5a4d1940f14f
Tim Voronov <ziflex@gmail.com>
2026-02-12 10:35:06 -0500

    Add benchmark tests for collect projection queries to evaluate performance

---
+++ test/integration/benchmarks/bench_collect_projection_test.go
@@ -0,0 +1,67 @@
+package benchmarks_test
+
+import "testing"
+
+const (
+	collectProjectionAllVars = `
+FOR i IN 1..200
+  LET a = i + 1
+  LET b = i + 2
+  COLLECT g = i % 5 INTO groups
+  RETURN groups
+`
+
+	collectProjectionKeep = `
+FOR i IN 1..200
+  LET a = i + 1
+  LET b = i + 2
+  COLLECT g = i % 5 INTO groups KEEP a, b
+  RETURN groups
+`
+
+	collectProjectionCustom = `
+FOR i IN 1..200
+  LET a = i + 1
+  LET b = i + 2
+  COLLECT g = i % 5 INTO groups = { a: a, b: b }
+  RETURN groups
+`
+
+	collectProjectionCount = `
+FOR i IN 1..200
+  COLLECT WITH COUNT INTO total
+  RETURN total
+`
+)
+
+func BenchmarkCollectProjection_AllVars_O0(b *testing.B) {
+	RunBenchmarkO0(b, collectProjectionAllVars)
+}
+
+func BenchmarkCollectProjection_AllVars_O1(b *testing.B) {
+	RunBenchmarkO1(b, collectProjectionAllVars)
+}
+
+func BenchmarkCollectProjection_Keep_O0(b *testing.B) {
+	RunBenchmarkO0(b, collectProjectionKeep)
+}
+
+func BenchmarkCollectProjection_Keep_O1(b *testing.B) {
+	RunBenchmarkO1(b, collectProjectionKeep)
+}
+
+func BenchmarkCollectProjection_Custom_O0(b *testing.B) {
+	RunBenchmarkO0(b, collectProjectionCustom)
+}
+
+func BenchmarkCollectProjection_Custom_O1(b *testing.B) {
+	RunBenchmarkO1(b, collectProjectionCustom)
+}
+
+func BenchmarkCollectProjection_Count_O0(b *testing.B) {
+	RunBenchmarkO0(b, collectProjectionCount)
+}
+
+func BenchmarkCollectProjection_Count_O1(b *testing.B) {
+	RunBenchmarkO1(b, collectProjectionCount)
+}
