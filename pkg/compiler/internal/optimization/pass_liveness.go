3f6391883e9c6ca8b86b56fe7db7c768f5d38bf1
Tim Voronov <ziflex@gmail.com>
2026-03-19 14:08:02 -0400

    chore: reorganize benchmark workflows and refactor mapsEqual function

--- pkg/compiler/internal/optimization/pass_liveness.go
+++ pkg/compiler/internal/optimization/pass_liveness.go
@@ -368,18 +368,3 @@ func computeUseDefForInstruction(inst bytecode.Instruction, info *LivenessInfo) 
 		info.Def[reg] = true
 	}
 }
-
-// mapsEqual checks if two register maps are equal
-func mapsEqual(a, b map[int]bool) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	for k := range a {
-		if !b[k] {
-			return false
-		}
-	}
-
-	return true
-}
