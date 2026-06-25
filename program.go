dc675ec41f10836d7fb5d3fa5b1738075fe48918
Tim Voronov <ziflex@gmail.com>
2026-06-25 19:59:59 -0400

    refactor: use aliases and consolidate artifact-related functions

--- program.go
+++ program.go
@@ -1,25 +1,20 @@
 package ferret
 
 import (
-	"github.com/MontFerret/ferret/v2/pkg/bytecode"
 	"github.com/MontFerret/ferret/v2/pkg/bytecode/artifact"
-	"github.com/MontFerret/ferret/v2/pkg/runtime"
 )
 
-// ArtifactOption is a type alias for artifact.Option, used to define functional options for artifact marshaling.
-type ArtifactOption = artifact.Option
+// ProgramArtifactOption is a type alias for artifact.Option, used to define functional options for artifact marshaling.
+type ProgramArtifactOption = artifact.Option
 
-// MarshalProgram serializes the given bytecode program into a byte slice using the provided artifact options.
-// Returns an error if the program is nil or fails during the marshaling process.
-func MarshalProgram(prog *bytecode.Program, opts ...ArtifactOption) ([]byte, error) {
-	if prog == nil {
-		return nil, runtime.Error(runtime.ErrInvalidArgument, "program cannot be nil")
-	}
+var (
+	// WithProgramFormat specifies an artifact format by applying the provided FormatID to the options configuration.
+	WithProgramFormat = artifact.WithFormat
 
-	return artifact.Marshal(prog, opts...)
-}
+	// MarshalProgram serializes the given bytecode program into a byte slice using the provided artifact options.
+	// Returns an error if the program is nil or fails during the marshaling process.
+	MarshalProgram = artifact.Marshal
 
-// UnmarshalProgram decodes a byte slice into a *bytecode.Program object or returns an error if unmarshaling fails.
-func UnmarshalProgram(data []byte) (*bytecode.Program, error) {
-	return artifact.Unmarshal(data)
-}
+	// UnmarshalProgram decodes a byte slice into a *bytecode.Program object or returns an error if unmarshaling fails.
+	UnmarshalProgram = artifact.Unmarshal
+)
